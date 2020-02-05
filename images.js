9e0d08ee1a270b8ac9539b53bace07ffd21fd274
Robert Bishop <rbishop2006@gmail.com>
2020-02-04 16:28:51 -0800

    fourth commit

---
+++ images.js
@@ -0,0 +1,24 @@
+var carouselImages = [
+  {
+    image_url: "https://wallpapersite.com/images/pages/pic_w/872.jpg",
+    title: "Watch Dogs",
+    photographer_id: "Dog201"
+  },
+  {
+    image_url:
+      "https://cdn.allwallpaper.in/wallpapers/1280x720/13655/video-games-assassins-creed-game-1280x720-wallpaper.jpg",
+    title: "Assassins Creed",
+    photographer_id: "Assassin20"
+  },
+  {
+    image_url:
+      "https://walldiskpaper.com/wp-content/uploads/2014/11/God-Of-War-Wallpaper-Android.jpg",
+    title: "God of War",
+    photographer_id: "GodofWar22"
+  }
+  // {
+  //   image_url: 'http://paperlief.com/images/tomb-raider-cast-wallpaper-3.jpg',
+  //   title: 'Tomb Raider',
+  //   photographer_id: 'Raider7'
+  // }
+]
