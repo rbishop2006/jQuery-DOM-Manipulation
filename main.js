25d987bbf5836f6e24633753f84156387525a61f
Robert Bishop <rbishop2006@gmail.com>
2020-06-24 10:05:32 -0700

    commented out carousel scripts

--- main.js
+++ main.js
@@ -1,24 +1,22 @@
-$(document).ready(function() {
-  $(".titles").on("click", function(e) {
+$(document).ready(function () {
+  $(".titles").on("click", function (e) {
     $(".content").removeClass("show")
 
-    $(this)
-      .find("+ .content")
-      .addClass("show")
+    $(this).find("+ .content").addClass("show")
   })
 })
 
-let html = carouselImages
+// let html = carouselImages
 
-  .map(function(item) {
-    return `
-    <div class="imageBox">
-        <img class="images" src="${item.image_url}"/>
-        <p class="title">${item.title}</p>
-        <p class="photographer_id">${item.photographer_id}</p>
-        </div>
-        `
-  })
-  .join("")
+//   .map(function(item) {
+//     return `
+//     <div class="imageBox">
+//         <img class="images" src="${item.image_url}"/>
+//         <p class="title">${item.title}</p>
+//         <p class="photographer_id">${item.photographer_id}</p>
+//         </div>
+//         `
+//   })
+//   .join("")
 
-document.querySelector(".slideShow").innerHTML = html
+// document.querySelector(".slideShow").innerHTML = html
